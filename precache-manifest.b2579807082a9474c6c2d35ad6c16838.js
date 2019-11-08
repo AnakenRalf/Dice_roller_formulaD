94fe33e95679bf60e62a734135521b71f8181eb8
Ruslan Ausiannikau <Anaken@gmail.com>
2019-11-08 22:52:10 +0200

    Updates

---
+++ precache-manifest.b2579807082a9474c6c2d35ad6c16838.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4844917c068f21a44033987d35a191",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4703bf7a86400d24838",
+    "url": "/static/css/2.9c7ff97b.chunk.css"
+  },
+  {
+    "revision": "d9a4b5e1cba4ef5707b8",
+    "url": "/static/css/main.6d9556d1.chunk.css"
+  },
+  {
+    "revision": "f4703bf7a86400d24838",
+    "url": "/static/js/2.5005afd7.chunk.js"
+  },
+  {
+    "revision": "d9a4b5e1cba4ef5707b8",
+    "url": "/static/js/main.27d10e5f.chunk.js"
+  },
+  {
+    "revision": "a6d1bc33365cefc38550",
+    "url": "/static/js/runtime-main.94d414e9.js"
+  }
+]);
